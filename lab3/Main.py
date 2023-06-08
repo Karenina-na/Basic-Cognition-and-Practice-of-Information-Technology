63e25a531afbe373d41e2f479a9e3d503d3474b8
Karenina-na <weizixiang0@outlook.com>
2023-06-08 20:02:54 +0800

    学生、课程和选课信息管理类

--- lab3/Main.py
+++ lab3/Main.py
@@ -2,9 +2,10 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5 import QtCore
 from UI.MainUI import Ui_StudentManagement
+from Obj import *
 
 
 class Main(QMainWindow):
@@ -14,6 +15,18 @@ class Main(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.studentButton.clicked.connect(self.studentRegister)
+
+        # 学生管理类
+        self.studentManager = StudentList()
+        # 课程管理类
+        self.courseManager = CourseList()
+        # 选课管理类
+        self.scManager = SelectCourseList()
+
+        # 加载信息
+        self.studentManager.load()
+        self.courseManager.load()
+        self.scManager.load()
 
 
     def studentRegister(self):
@@ -31,35 +44,31 @@ class Main(QMainWindow):
         studentAge = self.ui.Age_input.text()
         studentClass = self.ui.Class_input.text()
 
-        # 如果有空值，弹出提示
+        # 如果有空值，弹出警告
         if studentID == "":
             self.ui.ID_input.setFocus()
             self.ui.ID_input.setStyleSheet("border: 1px solid red;")
-            self.ui.statusbar.showMessage("学号不能为空")
             return
         else:
             self.ui.ID_input.setStyleSheet("")
         if studentName == "":
             self.ui.Name_input.setFocus()
             self.ui.Name_input.setStyleSheet("border: 1px solid red;")
-            self.ui.statusbar.showMessage("学生姓名不能为空")
             return
         else:
             self.ui.Name_input.setStyleSheet("")
         if studentGender is None:
-            self.ui.statusbar.showMessage("性别不能为空")
+            QMessageBox.warning(self, "错误", "性别不能为空")
             return
         if studentAge == "":
             self.ui.Age_input.setFocus()
             self.ui.Age_input.setStyleSheet("border: 1px solid red;")
-            self.ui.statusbar.showMessage("年龄不能为空")
             return
         else:
             self.ui.Age_input.setStyleSheet("")
         if studentClass == "":
             self.ui.Class_input.setFocus()
             self.ui.Class_input.setStyleSheet("border: 1px solid red;")
-            self.ui.statusbar.showMessage("班级不能为空")
             return
         else:
             self.ui.Class_input.setStyleSheet("")
