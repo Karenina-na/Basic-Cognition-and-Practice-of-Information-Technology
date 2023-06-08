f9d6e3fca43adce801398007c891adfb0ee1e911
Karenina-na <weizixiang0@outlook.com>
2023-06-08 19:59:01 +0800

    UI完成

--- lab3/Main.py
+++ lab3/Main.py
@@ -2,23 +2,99 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore
+from UI.MainUI import Ui_StudentManagement
+
+
+class Main(QMainWindow):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        self.ui = Ui_StudentManagement()
+        self.ui.setupUi(self)
+
+        self.ui.studentButton.clicked.connect(self.studentRegister)
+
+
+    def studentRegister(self):
+        """
+        输入学生信息，注册学生
+        """
+        # 获取学生信息
+        studentID = self.ui.ID_input.text()
+        studentName = self.ui.Name_input.text()
+        studentGender = None
+        if self.ui.manButton.isChecked():
+            studentGender = "男"
+        elif self.ui.womanButton.isChecked():
+            studentGender = "女"
+        studentAge = self.ui.Age_input.text()
+        studentClass = self.ui.Class_input.text()
+
+        # 如果有空值，弹出提示
+        if studentID == "":
+            self.ui.ID_input.setFocus()
+            self.ui.ID_input.setStyleSheet("border: 1px solid red;")
+            self.ui.statusbar.showMessage("学号不能为空")
+            return
+        else:
+            self.ui.ID_input.setStyleSheet("")
+        if studentName == "":
+            self.ui.Name_input.setFocus()
+            self.ui.Name_input.setStyleSheet("border: 1px solid red;")
+            self.ui.statusbar.showMessage("学生姓名不能为空")
+            return
+        else:
+            self.ui.Name_input.setStyleSheet("")
+        if studentGender is None:
+            self.ui.statusbar.showMessage("性别不能为空")
+            return
+        if studentAge == "":
+            self.ui.Age_input.setFocus()
+            self.ui.Age_input.setStyleSheet("border: 1px solid red;")
+            self.ui.statusbar.showMessage("年龄不能为空")
+            return
+        else:
+            self.ui.Age_input.setStyleSheet("")
+        if studentClass == "":
+            self.ui.Class_input.setFocus()
+            self.ui.Class_input.setStyleSheet("border: 1px solid red;")
+            self.ui.statusbar.showMessage("班级不能为空")
+            return
+        else:
+            self.ui.Class_input.setStyleSheet("")
+
+        print(studentID, studentName, studentGender, studentAge, studentClass)
+
+    def removeStudent(self):
+        pass
+
+    def queryStudent(self):
+        pass
+
+    def courseRegister(self):
+        pass
+
+    def removeCourse(self):
+        pass
+
+    def queryCourse(self):
+        pass
+
+    def SCRegister(self):
+        pass
+
+    def removeSC(self):
+        pass
+
 
 if __name__ == '__main__':
-    # 创建QApplication类的实例
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)
-    # 创建一个窗口
-    w = QWidget()
-    # 设置窗口尺寸   宽度300，高度150
-    w.resize(400, 200)
-    # 移动窗口
-    w.move(300, 300)
 
-    # 设置窗口的标题
-    w.setWindowTitle('第一个基于PyQt5的桌面应用')
-
+    # 创建主窗口
+    main = Main()
     # 显示窗口
-    w.show()
+    main.show()
 
-    # 进入程序的主循环，并通过exit函数确保主循环安全结束(该释放资源的一定要释放)
     sys.exit(app.exec_())
